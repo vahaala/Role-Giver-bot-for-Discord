cbf3c241f61a0d0f533be5a476e0f5d253bef691
Paweł Brzozowski <vahaala@gmail.com>
2021-06-15 22:07:18 +0200

    Role giver bot v2
    A big rewrite of the bot. Now adds and removes roles based on reactions, and the message's visual side is also much better.

--- role_giver_bot.py
+++ role_giver_bot.py
@@ -2,10 +2,12 @@ import discord #imports Discord module
 import os #imports OS module
 from discord.ext import commands #imports commands submodule from discord.ext
 from dotenv import load_dotenv #imports load_dotenv submodule from dotenv
+intents = discord.Intents.all()
+
 
 load_dotenv() #loads .env file, which stores our Discord bot token. You probably will need to make one yourself, in the same spot as the script.
 
-bot = commands.Bot(command_prefix = '!') #defines that this is a bot, and it's commands prefix
+bot = commands.Bot(command_prefix = '!', intents = intents) #defines that this is a bot, and it's commands prefix
 
 @bot.event #on bot event:
 async def on_ready(): #when bot is fully ready:
@@ -13,18 +15,43 @@ async def on_ready(): #when bot is fully ready:
 
 @bot.command() #sets up a bot command
 async def roles(ctx): #defines the command name, and it's arguments. Usage is !roles (prefix, then actual command without space inbetween. It's using the def name for it)
-    await ctx.channel.send("Here are available roles: \n" + ", ".join([str(r.name) for r in ctx.guild.roles if r.name not in ["@everyone", "Role Giver"]]) + "\nYou can give yourself desired role (and access to linked channels) by typing \"!addrole role_name\". One at a time please!") #sends a message to channel the bot command was activated in
+    embed = discord.Embed(title = "Roles list", description = "Here are available roles: ", color = discord.Color.gold()) #formats the bot message in embed style, looks nicely
+    global rls #defines a global list of roles
+    rls = [r.name for r in ctx.guild.roles if r.name not in ["@everyone", "Role Giver"]] #makes actual content for it
+    global emojis #defines a global list of emojis (reactions) available
+    emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"] #and also contents
+    for index, r in enumerate(rls): #iterates over the roles list
+        embed.add_field(name = f"{r}", value = f"React with {emojis[index]} emoji to give yourself a **{r}** role.", inline = False) #for each role, adds a new embed field, with emoji and role name inserted inside
+    global msg #defines a global variable of message
+    msg = await ctx.send(embed = embed) #sends a message as embed
+    for i in range(len(rls)): #adds appropriate number of reactions, equal to number of roles
+        await msg.add_reaction(emojis[i])
 
-@bot.command() #sets up another bot command, it is required for each command - if you do multiple defs in one @bot decorator, the bot will only properly recognize the first command.
-async def addrole(ctx, arg): #defines command name, context (as in, message) and it's arguments (what comes after the !addrole):
-    role_name = discord.utils.get(ctx.guild.roles, name = f"{arg}") #extracts role name from given message
-    await ctx.author.add_roles(role_name) #adds specified role to the user who invoked the command
-    await ctx.channel.send("Role added!") #prints a confirmation message in the channel
+@bot.event
+async def on_reaction_add(reaction, usr): #on added reaction:
+    message_id = msg.id #extracts id of bot message
+    if reaction.message.id != message_id: #compares if they're not the same - if the bot isn't responding to it's own
+        return
+    global roles_dict
+    roles_dict = {}  #dictionary of roles, with keys being emojis and roles being values
+    for i in range(len(rls)): #populates dictionary, effectively binding a role to emoji
+        roles_dict[emojis[i]] = rls[i] #takes first item from emoji list as a key, and then first item from roles list as a value
+    for i in roles_dict: #iterates over populated roles dictionary
+        role_name = discord.utils.get(usr.guild.roles, name = roles_dict.get(i)) #calculates correct role name
+        if reaction.emoji == i: #if someone clicked on a reaction:
+            if usr.bot: #don't let bot get the roles itself
+                pass
+            else:
+                await usr.add_roles(role_name, atomic = True) #adds role that is bound to clicked emoji
 
-@bot.command() #another command
-async def delrole(ctx, arg): #basically same as above command, just for removing roles instead of adding them
-    role_name = discord.utils.get(ctx.guild.roles, name = f"{arg}")
-    await ctx.author.remove_roles(role_name)
-    await ctx.channel.send("Role removed!")
+@bot.event
+async def on_reaction_remove(reaction, usr): #on revoked reaction (lots of code same as for adding reaction):
+    message_id = msg.id
+    if reaction.message.id != message_id:
+        return
+    for i in roles_dict:
+        role_name = discord.utils.get(usr.roles, name = roles_dict.get(i))
+        if reaction.emoji == i:
+            await usr.remove_roles(role_name, atomic = True)
 
 bot.run(os.getenv("DISCORD_TOKEN"))
